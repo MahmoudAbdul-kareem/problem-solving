e3a798b6fe8c17216ee2910ff7ae165b5ff744d5
MahmoudAbdul-kareem <mahmoudabdulkareemawad@gmail.com>
2023-08-16 15:30:12 +0300

    organizing repo directories

---
+++ templates/dir_temp/tmep.c
@@ -0,0 +1,46 @@
+/**                       Documentation section
+ =========================================================================
+  @file           : file_name.c                <<<<<<
+  @author         : MahmoudAbdul-kareem
+  @link           :                            <<<<<<
+ =========================================================================
+ */
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~ Includes Section Start ~~~~~~~~~~~~~~~~~~~~~~ */
+#include <stdio.h>
+#include <stdlib.h>
+/* ~~~~~~~~~~~~~~~~~~~~~~~ Includes Section End ~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~ Macro Section Start ~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~ Macro Section End ~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/* ~~~~~~~~~~~~~~~~~~ Global Variables Section Start ~~~~~~~~~~~~~~~~~~ */
+
+/* ~~~~~~~~~~~~~~~~~~ Global Variables  Section End ~~~~~~~~~~~~~~~~~~~ */
+
+
+/* ~~~~~~~~~~~~~~~~~ Global Decleration Section Start ~~~~~~~~~~~~~~~~~ */
+// variables:
+
+// prototypes:
+void file();
+/* ~~~~~~~~~~~~~~~~~~ Global Decleration Section End ~~~~~~~~~~~~~~~~~~ */
+
+
+int main()
+{
+    
+    return 0;
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~ Sub-Program Section Start ~~~~~~~~~~~~~~~~~~~~~ */
+void file()
+{
+    // freopen("input.txt", "rt", stdin);  // give input to terminal
+    freopen("output.txt", "wt", stdout);
+}
+/* ~~~~~~~~~~~~~~~~~~~~~ Sub-Program Section End ~~~~~~~~~~~~~~~~~~~~~~ */
